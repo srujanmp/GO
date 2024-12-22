011f6f69916688c6edd90a3b7e358a55a4f8abc9
Srujan M Padmashali <srujanmpadmshali@gmail.com>
2024-12-22 19:27:09 +0530

    http simplest webserver

--- app.go
+++ app.go
@@ -1,6 +1,15 @@
 package main
 
-import 
-func main(){
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello : %s", r.URL)
+	})
+
+	http.ListenAndServe(":8000", nil)
 }
