50f431b39ae491dd4a9fd1f58f03e1763d5863e5
Srujan M Padmashali <88569710+srujanmp@users.noreply.github.com>
2024-12-23 00:13:12 +0000

    final

--- app.go
+++ app.go
@@ -1,15 +1,37 @@
+// websockets.go
 package main
 
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
-func main() {
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello : %s", r.URL)
+func main() {
+	http.HandleFunc("/socket", func(w http.ResponseWriter, r *http.Request) {
+		conn, _ := upgrader.Upgrade(w, r, nil)
+		for{
+			_, msg, err:= conn.ReadMessage() //recieves msg
+			if err != nil {return}
+
+			fmt.Println(string(msg),"recieved")  //prints msg to console
+
+			err=conn.WriteMessage(websocket.TextMessage, []byte("pong")) //sends pong to frontend
+			if err != nil {return}
+			fmt.Println("pong sent")
+			
+		}
 	})
 
-	http.ListenAndServe(":8000", nil)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "websockets.html")
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go/test
 
 go 1.23.4
+
+require github.com/gorilla/websocket v1.5.3
